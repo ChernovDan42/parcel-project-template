7098f4e1221b6953155c102e58191de8b8b623e7
Dan Chernov <120645560+ChernovDan42@users.noreply.github.com>
2023-05-05 13:34:57 +0200

    finish 2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,38 @@
 
 import Player from '@vimeo/player';
+var _ = require('lodash')
 
 
-const player = new Player('handstick', {
+const player = new Player('vimeo-player', {
     id: 19231868,
     width: 640
 });
 
-player.on('timeupdate', function(data) {
-    duration: 61.857
-    percent: 0.049
-    seconds: 3.034
 
+const onPlay = function (data) {
+    console.log(data);
+    
+    localStorage.setItem("videoplayer-current-time",JSON.stringify(data.seconds))
+};
+
+
+player.on('timeupdate',_.throttle(onPlay,1000));
+
+
+const currentTimeVideo = Number(localStorage.getItem("videoplayer-current-time"));
+console.log(currentTimeVideo);
+
+
+player.setCurrentTime(currentTimeVideo).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
-
-
-
